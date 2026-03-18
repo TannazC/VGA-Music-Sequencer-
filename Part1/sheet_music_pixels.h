4a4dbcd7fb7728f74f2ff27a2e2b256cd0ce9a40
dareen-nasreldin <dareennasreldin@gmail.com>
2026-03-18 00:48:28 -0400

    fix: staff lines now properly display evenly, need to add treble clefs
     - No need for sheet_music_pixels.c or sheet_music_pixels.h

---
+++ Part1/sheet_music_pixels.h
@@ -0,0 +1,10 @@
+#ifndef SHEET_MUSIC_PIXELS_H
+#define SHEET_MUSIC_PIXELS_H
+
+#define SM_WIDTH  630
+#define SM_HEIGHT 480
+
+/* Bitmap data lives in sheet_music_pixels.c (placed in SDRAM by linker) */
+extern const unsigned char sheet_music_bitmap[];
+
+#endif
